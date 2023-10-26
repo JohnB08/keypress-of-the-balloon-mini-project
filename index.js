2a65d6bf63724ec015be51856ade4e05158ae3e3
JohnB08 <johnb08.kodehode@gmail.com>
2023-10-26 11:18:42 +0200

    square spawn random

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const balloonContainer = document.querySelector(".ballooncontainer");
 const spawnBalloon = () => {
   let balloon = document.createElement("div");
   balloon.classList.add("balloon");
+  balloon.style.top = `${Math.floor(Math.random() * 90)}%`;
+  balloon.style.left = `${Math.floor(Math.random() * 90)}%`;
   balloonContainer.appendChild(balloon);
 };
 setInterval(spawnBalloon, 5000);
