693035d97e101cc83b877e9006291b5e68df835c
JohnB08 <johnb08.kodehode@gmail.com>
2023-10-26 13:23:46 +0200

    added life function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const scoreContainer = document.querySelector(".scorecontainer h1");
+const scoreContainer = document.querySelector(".scorecontainer");
+const scoreCount = document.querySelector(".scorecontainer h1");
 const balloonContainer = document.querySelector(".ballooncontainer");
 const alphabet = [
   "A",
@@ -28,28 +29,66 @@ const alphabet = [
   "Y",
   "Z",
 ];
+const heartContainer = document.createElement("div");
+heartContainer.classList.add("heartcontainer");
+let startGameBtn = document.createElement("button");
+startGameBtn.classList.add("btn");
+startGameBtn.textContent = "Start Game!";
+balloonContainer.appendChild(startGameBtn);
 let score = 0;
+let balloonSpawner = null;
+let life = 5;
 const spawnBalloon = () => {
   let balloon = document.createElement("div");
   balloon.classList.add("balloon");
   //ballongene spawner med minst 5% luft mot kanten av kontaineren.
-  balloon.style.top = `${Math.floor(Math.random() * 90) + 5}%`;
-  balloon.style.left = `${Math.floor(Math.random() * 90) + 5}%`;
+  balloon.style.top = `${Math.floor(Math.random() * 85) + 5}%`;
+  balloon.style.left = `${Math.floor(Math.random() * 85) + 5}%`;
   let balloonLetter = document.createElement("p");
   balloonLetter.textContent =
     alphabet[Math.floor(Math.random() * alphabet.length)];
   balloonContainer.appendChild(balloon);
   balloon.appendChild(balloonLetter);
 };
-setInterval(spawnBalloon, 1500);
+const lifeCount = () => {
+  for (let i = 0; i < life; i++) {
+    const heart = document.createElement("img");
+    heart.src = "./img/life.svg";
+    heart.classList.add("heart");
+    heartContainer.appendChild(heart);
+  }
+};
+startGameBtn.addEventListener("click", (event) => {
+  life = 5;
+  score = 0;
+  startGameBtn.remove("");
+  balloonSpawner = setInterval(spawnBalloon, 1500);
+  scoreContainer.appendChild(heartContainer);
+  lifeCount();
+});
 document.addEventListener("keydown", (event) => {
   let balloons = document.querySelectorAll(".balloon");
   let letters = document.querySelectorAll("p");
+  let prevScore = score;
   for (let i = 0; i < balloons.length; i++) {
     if (event.code === `Key${letters[i].textContent}`) {
       balloons[i].remove();
       score++;
-      scoreContainer.textContent = `Score: ${score}`;
+      scoreCount.textContent = `Score: ${score}`;
     }
+  }
+  if (prevScore === score) {
+    let hearts = document.querySelectorAll(".heart");
+    hearts[life - 1].remove();
+    life--;
+    console.log(life);
+  }
+  if (life === 0) {
+    clearInterval(balloonSpawner);
+    let ballons = document.querySelectorAll(".balloon");
+    for (let ballon of ballons) {
+      ballon.remove();
+    }
+    balloonContainer.appendChild(startGameBtn);
   }
 });
