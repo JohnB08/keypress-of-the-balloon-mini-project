f9f38f52765d13199ae3046dd10ab4bec0c3975c
JohnB08 <johnb08.kodehode@gmail.com>
2023-10-26 11:33:58 +0200

    added simple functionality

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const scoreContainer = document.querySelector(".scorecontainer");
 const balloonContainer = document.querySelector(".ballooncontainer");
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 const spawnBalloon = () => {
   let balloon = document.createElement("div");
   balloon.classList.add("balloon");
   balloon.style.top = `${Math.floor(Math.random() * 90)}%`;
   balloon.style.left = `${Math.floor(Math.random() * 90)}%`;
+  let balloonLetter = document.createElement("p");
+  balloonLetter.textContent =
+    alphabet[Math.floor(Math.random() * alphabet.length)];
   balloonContainer.appendChild(balloon);
+  balloon.appendChild(balloonLetter);
 };
-setInterval(spawnBalloon, 5000);
+setInterval(spawnBalloon, 3000);
+document.addEventListener("keydown", (event) => {
+  let balloons = document.querySelectorAll(".balloon");
+  let letters = document.querySelectorAll("p");
+  for (let i = 0; i < balloons.length; i++) {
+    if (event.code === `Key${letters[i].textContent}`) balloons[i].remove();
+  }
+});
