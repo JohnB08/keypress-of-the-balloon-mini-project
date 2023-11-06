b6a5086843bb93719561860f7b2d2948bf13449f
JohnB08 <johnb08.kodehode@gmail.com>
2023-11-06 20:05:33 +0100

    Generell koderydding

--- index.js
+++ index.js
@@ -51,6 +51,8 @@ scoreContainer.appendChild(highScoreTracker);
 
 scoreContainer.appendChild(heartContainer);
 
+mobileCheck();
+
 showMenu();
 
 //adder styling til det som trenger styling.
@@ -76,6 +78,26 @@ const baseValues = {
   currentHearts: [],
 };
 
+//predefinerer variabler jeg trenger til spillet.
+//Gir alle "default" verdier så de blir lastet inn i memory.
+//bruker dekonstruering av baseValues objectet for å sette alle variablene til verdien de har i baseValues.
+
+let {
+  score,
+  highScore,
+  balloonSpawner,
+  lifeTimer,
+  life,
+  maxBalloon,
+  stopped,
+  totalBalloonCount,
+  gameObjects,
+  currentHearts,
+} = baseValues;
+gameObjects.hiddenInput = {};
+gameObjects.balloons = {};
+gameObjects.hiddenInput.isActive = false;
+
 //Lager et object for sound effects, og hvor de er lagret, så jeg kan hente det inn senere.
 const soundElements = {
   backgroundMusic: {
@@ -89,56 +111,6 @@ const soundElements = {
     },
   },
 };
-//predefinerer variabler jeg trenger til spillet.
-//Gir alle "default" verdier så de blir lastet inn i memory.
-score = baseValues.score;
-highScore = baseValues.highScore;
-balloonSpawner = baseValues.balloonSpawner;
-lifeTimer = baseValues.lifeTimer;
-life = baseValues.life;
-maxBalloon = baseValues.maxBalloon;
-stopped = baseValues.stopped;
-totalBalloonCount = baseValues.totalBalloonCount;
-gameObjects = baseValues.gameObjects;
-currentHearts = baseValues.currentHearts;
-gameObjects.hiddenInput = {};
-gameObjects.balloons = {};
-gameObjects.hiddenInput.isActive = false;
-
-/* ON LOAD FUNCTIONS */
-
-//Funksjon som lager element, tar in to ting:
-//string som er hvilken type element, og et object array med propertynavn -> property value, pluss initial CSS class, og hvor den skal appendes.
-const makeElement = (type, properties, className) => {
-  const element = document.createElement(type);
-
-  //tar alle keys og values og gjør de om til key/value arrays.
-  const elementProperties = Object.entries(properties);
-  elementProperties.forEach((property) => {
-    //dekonstrukter hvert key/value array til to variabler:
-    const [propertyName, propertyValue] = property;
-    //setter hver property til elementet:
-    //bruker bracketnotations, siden bracketnotation tar in en string i steden for et keyword
-    //da kan vi bruke propertyName variablen, siden den alltid vil være en string.
-    element[propertyName] = propertyValue;
-  });
-  element.classList.add(className);
-  return element;
-};
-
-//prøver å gjøre spillet compatible på tlf med en hack
-//kjører en funksjon på load som ser om viewporten er smal nok til å være en mobilskjerm, og lager en skjult inputfield under ballooncontainer.
-const mobileCheck = () => {
-  if (window.innerWidth > 600) return;
-  else {
-    let hiddenInput = makeElement("input", { type: "text" }, "hiddenInput");
-    gameObjects.hiddenInput.inputEl = hiddenInput;
-    gameObjects.hiddenInput.isActive = true;
-    balloonContainer.appendChild(hiddenInput);
-  }
-};
-mobileCheck();
-console.log(document);
 
 //lager bakgrunnsmusikkElementet
 soundElements.backgroundMusic.audioEl = makeElement("audio", {
@@ -153,14 +125,11 @@ soundElements.soundEffects.error.audioEl = makeElement("audio", {
 //Her Lager jeg difficultyselection + alle options i en for Each loop.
 Object.keys(difficultySelection).forEach((difficulty) => {
   difficultySelector.appendChild(
-    makeElement(
-      "option",
-      {
-        textContent: difficultySelection[difficulty].text,
-        value: Object.keys(difficultySelection).indexOf(difficulty),
-      },
-      "difficulty"
-    )
+    makeElement("option", {
+      textContent: difficultySelection[difficulty].text,
+      value: Object.keys(difficultySelection).indexOf(difficulty),
+      className: "difficulty",
+    })
   );
 });
 //prøver å hente highscore fra local storage.
@@ -173,58 +142,66 @@ if (!localStorage.getItem("highScore")) {
   highScore = savedHighScore;
 }
 
-/* HP DISPLAY AND LIFE MANIPULATION */
+/* Utility og MediaQuery */
 
-//Displayer hjerteSVG over balooncontainer basert på hvor mange liv man starter med.
-const lifeCount = () => {
-  for (let i = 0; i < life; i++) {
-    let heart = makeElement("img", { src: "./img/life.svg" }, "heart");
-    heartContainer.appendChild(heart);
-    //pusher det nye elementet til currentHearts array.
-    currentHearts.push(heart);
+//Funksjon som lager element, tar in to ting:
+//string som er hvilken type element, og et object array med propertynavn -> property value, pluss initial CSS class, og hvor den skal appendes.
+function makeElement(type, properties) {
+  const element = document.createElement(type);
+
+  //tar alle keys og values og gjør de om til key/value arrays.
+  const elementProperties = Object.entries(properties);
+  elementProperties.forEach((property) => {
+    //dekonstrukter hvert key/value array til to variabler:
+    const [propertyName, propertyValue] = property;
+    //setter hver property til elementet:
+    //bruker bracketnotations, siden bracketnotation tar in en string i steden for et keyword
+    //da kan vi bruke propertyName variablen, siden den alltid vil være en string.
+    element[propertyName] = propertyValue;
+  });
+  return element;
+}
+
+//lager en funksjon for å appende knapp og selector.
+function showMenu() {
+  balloonContainer.appendChild(startGameBtn);
+  balloonContainer.appendChild(difficultySelector);
+}
+
+//fjerner knapper og select fra skjermen.
+function removeMenu() {
+  startGameBtn.remove();
+  difficultySelector.remove();
+}
+
+//prøver å gjøre spillet compatible på tlf med en hack
+//kjører en funksjon på load som ser om viewporten er smal nok til å være en mobilskjerm, og lager en skjult inputfield under ballooncontainer.
+function mobileCheck() {
+  if (window.innerWidth > 600) return;
+  else {
+    let hiddenInput = makeElement("input", {
+      type: "text",
+      className: "hiddenInput",
+    });
+    gameObjects.hiddenInput.inputEl = hiddenInput;
+    gameObjects.hiddenInput.isActive = true;
+    balloonContainer.appendChild(hiddenInput);
   }
-};
+}
 
-//funksjon som fjerner liv og hjerter.
-const removeLife = () => {
-  //finner alle hjerteikonene som er igjen.
-  let hearts = currentHearts;
-  //fjerner en av de.
-  hearts[0].remove();
-  //prøvde å bruke push pop her, men den slet med å finne rett html element uten loop, hvis jeg gjorde det sånn. Nå trenger jeg ikke en loop.
-  hearts.shift();
-  life--;
-  //skjekker om vi har tapt.
-  if (life === 0) noLife();
-};
-
-//denne funksjonen kjører kun når life har blitt 0
-const noLife = () => {
-  //her stopper jeg begge intervallene.
-  clearInterval(balloonSpawner);
-  clearInterval(lifeTimer);
-  //jeg fjerner alle balloons som finnes.
-  let balloons = document.querySelectorAll(".balloon");
-  balloons.forEach((balloon) => balloon.remove());
-  //resetter balloon objektet.
-  gameObjects.balloons = {};
-  //skjekker om det er kommet en ny high score.
-  if (score > highScore) saveHighScore();
-  //Viser menyen igjen og setter stopped til true, sånn at alle keypress utenom enter blir ignorert.
-  showMenu();
-  //soundElements.backgroundMusic.audioEl.pause();
-  stopped = true;
-};
-
-/* RESET, RESTARTS & UTILITY */
+/* RESET, RESTART */
 
 //Funksjon som resetter spillet ved spillstart.
 function reset() {
   //setter life, time og maxBalloon basert på difficulty objekt.
-  let difficulty = Object.keys(difficultySelection)[difficultySelector.value];
-  life = difficultySelection[difficulty].maxLife;
-  maxBalloon = difficultySelection[difficulty].maxBalloon;
-  time = difficultySelection[difficulty].time;
+  let difficulty =
+    difficultySelection[
+      Object.keys(difficultySelection)[difficultySelector.value]
+    ];
+  console.log(difficulty);
+  life = difficulty.maxLife;
+  maxBalloon = difficulty.maxBalloon;
+  time = difficulty.time;
   //resetter score og totalBalloonCount til 0
   score = baseValues.score;
   totalBalloonCount = baseValues.totalBalloonCount;
@@ -237,36 +214,76 @@ function reset() {
 }
 
 //Lagrer ny highscore i localstorage hvis det har skjedd i noLife().
-const saveHighScore = () => {
+function saveHighScore() {
   highScore = score;
   localStorage.removeItem("highScore");
   localStorage.setItem("highScore", JSON.stringify(highScore));
   highScoreTracker.textContent = `HighScore: ${highScore}`;
-};
-
-//setter knapp og select inn på skjermen.
-function showMenu() {
-  balloonContainer.appendChild(startGameBtn);
-  balloonContainer.appendChild(difficultySelector);
 }
-
-//fjerner knapper og select fra skjermen.
-function removeMenu() {
-  startGameBtn.remove();
-  difficultySelector.remove();
-}
+//Neste prosjekt, se om jeg kan sette forskjellige highscores for forskjellige difficulties. Tror jeg kan hente .value fra selector, sette selector."difficultykey" som keyword.
+//Så hente det ut.
 
 //denne funksjonen kjører når spillet blir startet.
-const gameStart = () => {
+function gameStart() {
   reset();
   //setter life, time og maxballoons til det som er bestemt av vanskelighetsgraden
-  lifeCount();
+  displayLife();
   //fjerner startknappen og difficulty selector
   removeMenu();
   //soundElements.backgroundMusic.audioEl.play();
-};
+}
 
 /* BALLOON MANIPULATON */
+
+//spawnBalloon funksjon. Lager en firkant med tekst i, og plasserer den en tilfeldig plass i balooncontainer elementet.
+function spawnBalloon() {
+  //Genererer en random uppercase bokstav. Her bruker jeg toString(36).
+  //enkelt forklart så bestemmer toString() når 10 skal være 10, når er man ferdig å telle til ti.
+  //Den teller først gjennom alle tall fra 0-9, så alle små bokstaver a-z, før den igjen starter på 0. totalt 36 tegn.
+  //tallet inni () i toString() bestemmer hvor langt den skal telle før den treffer 0.
+  //toString(36) betyr at jeg vil at den skal telle gjennom alle tegnene. Jeg setter +10 Math.random stykket, sånn at den alltid hopper over tallene 0-9.
+  //bruker toUpperCase fordi jeg vil at bokstaven skal være tydlig.
+  let randomLetter = (Math.floor(Math.random() * 26) + 10)
+    .toString(36)
+    .toUpperCase();
+  //genererer en random x koordinat
+  let xCoordinate = Math.floor(Math.random() * 80) + 5;
+  //genererer en random y koordinat
+  //passer på at det alltid er litt luft fra kanten.
+  let yCoordinate = Math.floor(Math.random() * 80) + 5;
+  //kjører makeElement funksjonen, og gir div class balloon, og random x/y coordinater.
+  let balloon = makeElement("div", {
+    style: `top: ${yCoordinate}%; left: ${xCoordinate}%;`,
+    textContent: randomLetter,
+    className: "balloon",
+  });
+  balloonContainer.appendChild(balloon);
+  totalBalloonCount++;
+  //Lager en if else, for å se om balloon med bokstav er blitt spawnet allerede.
+  //Denne if/else statementen + objectet som lages, gjør at jeg kan ha samme funksjonalitet som før rewrite
+  //uten å måtte querySelectorAll etter .balloon classen som tidligere.
+  if (!gameObjects.balloons[balloon.textContent]) {
+    //lager et object, i gameObjects for denne bokstaven. Sender inn et element som er et Array av alle elementer hittil spawnet.
+    gameObjects.balloons[balloon.textContent] = {
+      balloonElements: [balloon],
+    };
+  } else {
+    //hvis objectet allerede er spawnet, pusher jeg det nye elementet til balloonElements Arrayet.
+    gameObjects.balloons[balloon.textContent].balloonElements.push(balloon);
+  }
+}
+
+//funksjon som kjøres hvis en balloon er funnet.
+function balloonSelector(letter) {
+  //Jeg vil bare fjerne en og en balloon. Gjør dette ved å hente elementArrayet mitt i gameObjects objectet.
+  let balloons = gameObjects.balloons[letter].balloonElements;
+  //sender første arrayet inn i balloonAnimation.
+  balloonAnimation(balloons[0]);
+  //fjerner det første elementet i arrayet.
+  balloons.shift();
+  totalBalloonCount--;
+  return;
+}
 
 //ved å gjøre balloon animation async, så kan balloonRemover "promise" en "resolve" før balloonAnimation er ferdig.
 //tar inn en ballong som parrameter.
@@ -299,59 +316,9 @@ const balloonChecker = () => {
   }
 };
 
-//funksjon som kjøres hvis en balloon er funnet.
-const balloonSelector = (letter) => {
-  //Jeg vil bare fjerne en og en balloon. Gjør dette ved å hente elementArrayet mitt i gameObjects objectet.
-  let balloons = gameObjects.balloons[letter].balloonElements;
-  //sender første arrayet inn i balloonAnimation.
-  balloonAnimation(balloons[0]);
-  //fjerner det første elementet i arrayet.
-  balloons.shift();
-  totalBalloonCount--;
-  return;
-};
-
-//spawnBalloon funksjon. Lager en firkant med tekst i, og plasserer den en tilfeldig plass i balooncontainer elementet.
-const spawnBalloon = () => {
-  //Genererer en random uppercase bokstav.
-  let randomLetter = (Math.floor(Math.random() * 26) + 10)
-    .toString(36)
-    .toUpperCase();
-  //genererer en random x koordinat
-  let xCoordinate = Math.floor(Math.random() * 80) + 5;
-  //genererer en random y koordinat
-  //passer på at det alltid er litt luft fra kanten.
-  let yCoordinate = Math.floor(Math.random() * 80) + 5;
-  //kjører makeElement funksjonen, og gir div class balloon, og random x/y coordinater.
-  let balloon = makeElement(
-    "div",
-    {
-      style: `top: ${yCoordinate}%; left: ${xCoordinate}%;`,
-      textContent: randomLetter,
-    },
-    "balloon"
-  );
-  balloonContainer.appendChild(balloon);
-  totalBalloonCount++;
-  //Lager en if else, for å se om balloon med bokstav er blitt spawnet allerede.
-  //Denne if/else statementen + objectet som lages, gjør at jeg kan ha samme funksjonalitet som før rewrite
-  //uten å måtte querySelectorAll etter .balloon classen som tidligere.
-  if (!gameObjects.balloons[balloon.textContent]) {
-    //lager et object, i gameObjects for denne bokstaven. Sender inn et element som er et Array av alle elementer hittil spawnet.
-    gameObjects.balloons[balloon.textContent] = {
-      balloonElements: [balloon],
-    };
-  } else {
-    //hvis objectet allerede er spawnet, pusher jeg det nye elementet til balloonElements Arrayet.
-    gameObjects.balloons[balloon.textContent].balloonElements.push(balloon);
-  }
-  console.log(gameObjects.balloons[balloon.textContent].balloonElements);
-};
-
 //hovedfunksjon for spillet. Sammenligner knapper og ballongcontent, og ser om ballonger skal fjernes.
-function gameEvent(keyStroke) {
-  let letter = keyStroke.toUpperCase();
-  //skjekker om en balloon med den teksten i det hele tatt er blitt spawna.
+function gameEvent(letter) {
+  //skjekker om en balloon med den teksten i det hele tatt er blitt spawna, eller om det er noen igjen i element arrayet.
   if (
     !gameObjects.balloons[letter] ||
     gameObjects.balloons[letter].balloonElements.length === 0
@@ -366,10 +333,56 @@ function gameEvent(keyStroke) {
   }
 }
 
+/* HP DISPLAY AND LIFE MANIPULATION */
+
+//Displayer hjerteSVG over balooncontainer basert på hvor mange liv man starter med.
+function displayLife() {
+  for (let i = 0; i < life; i++) {
+    let heart = makeElement("img", {
+      src: "./img/life.svg",
+      className: "heart",
+    });
+    heartContainer.appendChild(heart);
+    //pusher det nye elementet til currentHearts array.
+    currentHearts.push(heart);
+  }
+}
+
+//funksjon som fjerner liv og hjerter.
+function removeLife() {
+  //finner alle hjerteikonene som er igjen.
+  let hearts = currentHearts;
+  //fjerner en av de.
+  hearts[0].remove();
+  //prøvde å bruke push pop her, men den slet med å finne rett html element uten loop, hvis jeg gjorde det sånn. Nå trenger jeg ikke en loop.
+  hearts.shift();
+  life--;
+  //skjekker om vi har tapt.
+  if (life === 0) noLife();
+}
+
+//denne funksjonen kjører kun når life har blitt 0
+function noLife() {
+  //her stopper jeg begge intervallene.
+  clearInterval(balloonSpawner);
+  clearInterval(lifeTimer);
+  //jeg fjerner alle balloons som finnes.
+  let balloons = document.querySelectorAll(".balloon");
+  balloons.forEach((balloon) => balloon.remove());
+  //resetter balloon objektet.
+  gameObjects.balloons = {};
+  //skjekker om det er kommet en ny high score.
+  if (score > highScore) saveHighScore();
+  //Viser menyen igjen og setter stopped til true, sånn at alle keypress utenom enter blir ignorert.
+  showMenu();
+  //soundElements.backgroundMusic.audioEl.pause();
+  stopped = true;
+}
+
 /* EVENT LISTENERS */
 
 //Hvis man bruker knappen for å starte spillet.
-startGameBtn.addEventListener("click", (event) => {
+startGameBtn.addEventListener("click", () => {
   stopped = false;
   //prøver å lage en skjult input, som knappen fokuserer på hvis man er på mobiltlf.
   if (window.innerWidth < 600) {
@@ -378,21 +391,7 @@ startGameBtn.addEventListener("click", (event) => {
   gameStart();
 });
 
-//legger på en eventListener i tilfelle hiddenInput er laget.
-if (gameObjects.hiddenInput.isActive) {
-  gameObjects.hiddenInput.inputEl.addEventListener(
-    "beforeinput",
-    (touchedKey) => {
-      gameEvent(touchedKey.data.toLowerCase());
-    }
-  );
-  //eventlistener på balloonContainer i tilfelle bruker klikker vekk tastaturet.
-  balloonContainer.addEventListener("click", () =>
-    gameObjects.hiddenInput.inputEl.focus({ preventScroll: true })
-  );
-}
-
-//Legger på en eventListener til hele dokumentet. Denne ser etter tastetrykk.
+//Legger på en eventListener til hele dokumentet. Denne ser etter tastetrykk. Blir ignorert hvis man er på mobiltlf.
 document.addEventListener("keydown", (keyStroke) => {
   //Hvis spillet er stoppet, men enter er IKKE trykket, gjør ingenting.
   if (
@@ -405,5 +404,23 @@ document.addEventListener("keydown", (keyStroke) => {
     stopped = false;
     gameStart();
     //Hvis spillet er startet, kjør gameEvent for hver knapp.
-  } else gameEvent(keyStroke.key);
+    //bruker toUppercase for å normalisere inputen til stor bokstav.
+  } else gameEvent(keyStroke.key.toUpperCase());
 });
+
+/* EVENT LISTENER FOR MOBIL */
+
+//legger på en eventListener i tilfelle hiddenInput er laget.
+if (gameObjects.hiddenInput.isActive) {
+  gameObjects.hiddenInput.inputEl.addEventListener(
+    "beforeinput",
+    (touchedKey) => {
+      //bruker toUpperCase her også, bare for å standarisere input.
+      gameEvent(touchedKey.data.toUpperCase());
+    }
+  );
+  //eventlistener på balloonContainer i tilfelle bruker klikker vekk tastaturet.
+  balloonContainer.addEventListener("click", () =>
+    gameObjects.hiddenInput.inputEl.focus({ preventScroll: true })
+  );
+}
